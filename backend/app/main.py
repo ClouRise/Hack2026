b450c1f4f28c47cc6563d8409de118670c2d8c29
Boris <mr.clourise@gmail.com>
2026-03-21 00:32:28 +0300

    auth.py+create_image

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
+app.mount("/media", StaticFiles(directory="media"), name="media")
 
 @app.get("/")
 def read_root():
