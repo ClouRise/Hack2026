20df33e837c303ad69d393397e2ba1904cbf94d1
Timur <kirichenkotimur@gmail.com>
2026-03-22 11:05:54 +0300

    last commit of branch
    hi?

--- backend/app/migrations/env.py
+++ backend/app/migrations/env.py
@@ -1,7 +1,9 @@
 import asyncio
 from logging.config import fileConfig
+
 from app.db.base import Base
 from app import models
+
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
